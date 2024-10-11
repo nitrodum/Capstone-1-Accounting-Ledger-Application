d88d95f8cae8a76fc22691eb4093a235c5716052
Nima_Lama <nitrodum@yahoo.com>
2024-10-11 14:12:37 -0700

    Added method to display home screen

--- AccountingLedgerApplication/src/main/java/com/pluralsight/AccountingLedger.java
+++ AccountingLedgerApplication/src/main/java/com/pluralsight/AccountingLedger.java
@@ -11,22 +11,38 @@ public class AccountingLedger {
 
     public static void main(String[] args) {
         loadLedger();
+        homeScreen();
+    }
+
+    static void homeScreen() {
+        System.out.println("Welcome to your account!\n" +
+                "Please enter the letter corresponding to the command you would like to perform.\n" +
+                "D) Add Deposit\n" +
+                "P) Make Payment\n" +
+                "L) View Ledger\n" +
+                "X) Exit"
+        );
     }
 
     static void loadLedger() {
         try {
             BufferedReader bufferedReader = new BufferedReader(new FileReader("transactions.csv"));
-            String input;
+            String input = bufferedReader.readLine();
 
             while ((input = bufferedReader.readLine()) != null) {
                 String[] data = input.split("\\|");
                 Transaction t = new Transaction(LocalDate.parse(data[0]), LocalTime.parse(data[1]), data[2], data[3], Float.parseFloat(data[4]));
                 ledger.add(t);
             }
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             System.out.println("Error reading file!");
             e.printStackTrace();
+        }
+    }
+
+    static void testLedger() {
+        for (Transaction t : ledger) {
+            System.out.println("Date: " + t.getDate() + " Time: " + t.getTime() + " Description: " + t.getDescription() + " Vendor: " + t.getVendor() + " Amount: " + t.getAmount());
         }
     }
 }
