b0ab5a182a27a3e2c5f509421510552423a1c528
Nima_Lama <nitrodum@yahoo.com>
2024-10-11 15:07:30 -0700

    added method for handling payment and deposit

--- AccountingLedgerApplication/src/main/java/com/pluralsight/AccountingLedger.java
+++ AccountingLedgerApplication/src/main/java/com/pluralsight/AccountingLedger.java
@@ -1,11 +1,13 @@
 package com.pluralsight;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-import java.util.Locale;
 import java.util.Scanner;
 
 public class AccountingLedger {
@@ -18,25 +20,32 @@ public class AccountingLedger {
         while (running) {
             homeScreen();
         }
+        scanner.close();
     }
 
     static void homeScreen() {
-        System.out.println("Welcome to your account!\n" +
+        System.out.println(LocalDate.now());
+        System.out.println(LocalTime.now());
+        testLedger();
+        System.out.println(
+                "*******************************************************************************\n" +
+                "Welcome to your account!\n" +
                 "Please enter the letter corresponding to the command you would like to perform.\n" +
                 "D) Add Deposit\n" +
                 "P) Make Payment\n" +
                 "L) View Ledger\n" +
-                "X) Exit"
+                "X) Exit\n" +
+                "*******************************************************************************"
         );
 
         String input = scanner.nextLine().toUpperCase();
 
         switch (input) {
             case "D" :
-                System.out.println("Deposit not implemented yet!");
+                addTransaction(true);
                 break;
             case "P":
-                System.out.println("Payment not implemented yet!");
+                addTransaction(false);
                 break;
             case "L":
                 System.out.println("Ledger Screen not implemented yet!");
@@ -47,6 +56,46 @@ public class AccountingLedger {
                 break;
             default:
                 System.out.println("Invalid Option");
+        }
+    }
+
+    static void addTransaction(boolean deposit) {
+        String action;
+        if (deposit) {
+            action = "deposit";
+        } else {
+            action = "payment";
+        }
+
+        System.out.println("Please enter the details for the " + action + "\n" +
+                "Description: \n");
+        String description = scanner.nextLine();
+
+        System.out.println("Vendor name: ");
+        String vendor = scanner.nextLine();
+
+        System.out.println("Amount: ");
+        float amount = scanner.nextFloat();
+        scanner.nextLine();
+
+        if (!deposit) {
+            amount *= -1;
+        }
+
+        Transaction t = new Transaction(LocalDate.now(), LocalTime.now(), description, vendor, amount);
+        ledger.add(t);
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+
+        try {
+            FileWriter writer = new FileWriter("transactions.csv", true);
+            BufferedWriter bufferedWriter = new BufferedWriter(writer);
+            bufferedWriter.newLine();
+            bufferedWriter.write(LocalDate.now().toString() + "|" + LocalTime.now().format(formatter) + "|" + description + "|" + vendor + "|" + amount);
+            bufferedWriter.close();
+        } catch (Exception e) {
+            System.out.println("Error writing to file!");
+            e.printStackTrace();
         }
     }
 
